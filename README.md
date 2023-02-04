bff1e109da45021c639ae00e58bb206e5dc938af
daseul.kim <da91love@gmail.com>
2023-02-04 23:10:16 +0900

    [update]summary per, por, psr update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # aipscm-bizapi
-
+- GDP 국가 지표체계 자료: https://www.index.go.kr/unify/idx-info.do?idxCd=5001
+- M2 통계청 자료: https://ecos.bok.or.kr/#/SearchStat
--- common/util/create_market_summary_data.py
+++ common/util/create_market_summary_data.py
@@ -4,6 +4,7 @@ project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from common.util.EconoIndicator import EconoIndicator
 from common.util.Validation import Validation
 from common.const.KEY_NAME import KEY_NAME
+from common.const.COMM import PERIOD_UNIT
 import pydash as _
 
 def create_market_summary_data(period_data: list, period_unit: str):
@@ -31,6 +32,7 @@ def create_market_summary_data(period_data: list, period_unit: str):
             KEY_NAME['NP_CTRL']: 0,
         }
 
+        # Sum all data by period
         for s_data in period_data_by_group[period]:
             mv = s_data.get(KEY_NAME['MV'])
             sales = s_data.get(KEY_NAME['SALES'])
@@ -45,15 +47,56 @@ def create_market_summary_data(period_data: list, period_unit: str):
 
                 sum_data['tg_cmp_nb'] += 1
 
-        # Get psr por per
-        sum_data[KEY_NAME['PSR']] = EconoIndicator.get_mltp(sum_data[KEY_NAME['MV']], sum_data[KEY_NAME['SALES']])
-        sum_data[KEY_NAME['POR']] = EconoIndicator.get_mltp(sum_data[KEY_NAME['MV']], sum_data[KEY_NAME['OP']])
-        sum_data[KEY_NAME['PER']] = EconoIndicator.get_mltp(sum_data[KEY_NAME['MV']], sum_data[KEY_NAME['NP_CTRL']])
-
+        # Add period to sum_data
         sum_data[KEY_NAME['PERIOD']] = period
 
-        # 해당 Period에 대상 모집단 Company가 100개 이상일 시
+        # 해당 Period에 대상 모집단 Company가 100개 이상일 시에 한해 결과값으로 인정
+        # 100개 이하일 시 데이터 부족으로 탈락
         if sum_data['tg_cmp_nb'] > 100:
             result.append(sum_data)
+
+        # Get psr por per
+        # sales_4prd = EconoIndicator.get_4prd_sum(KEY_NAME['SALES'], )
+
+        # sum_data[KEY_NAME['PSR']] = EconoIndicator.get_mltp(sum_data[KEY_NAME['MV']], sum_data[KEY_NAME['SALES']])
+        # sum_data[KEY_NAME['POR']] = EconoIndicator.get_mltp(sum_data[KEY_NAME['MV']], sum_data[KEY_NAME['OP']])
+        # sum_data[KEY_NAME['PER']] = EconoIndicator.get_mltp(sum_data[KEY_NAME['MV']], sum_data[KEY_NAME['NP_CTRL']])
+
+    # Create temp_result to create multiple data
+    # 멀티플 데이터 계산 시, 회사수가 다른 문제가 발생하여 각 매출, 영업이익, 당기순이익을 회사수로 나눈 값을 멀티플 재료로 활용
+    temp_result = []
+    for data in result:
+        temp_result.append({
+            KEY_NAME['MV']: data[KEY_NAME['MV']] / data['tg_cmp_nb'],
+            KEY_NAME['SALES']: data[KEY_NAME['SALES']] / data['tg_cmp_nb'],
+            KEY_NAME['OP']: data[KEY_NAME['OP']] / data['tg_cmp_nb'],
+            KEY_NAME['NP_CTRL']: data[KEY_NAME['NP_CTRL']] / data['tg_cmp_nb'],
+            KEY_NAME['PERIOD']: data[KEY_NAME['PERIOD']]
+        })
+
+    for data in temp_result:
+        tg_period = data[KEY_NAME['PERIOD']]
+        tg_period_data_in_result = _.find(result, {KEY_NAME['PERIOD']: tg_period})
+
+        PSR = None
+        POR = None
+        PER = None
+        if period_unit == PERIOD_UNIT['YEAR']:
+            PSR = EconoIndicator.get_mltp(data[KEY_NAME['MV']], data[KEY_NAME['SALES']])
+            POR = EconoIndicator.get_mltp(data[KEY_NAME['MV']], data[KEY_NAME['OP']])
+            PER = EconoIndicator.get_mltp(data[KEY_NAME['MV']], data[KEY_NAME['NP_CTRL']])
+
+        elif period_unit == PERIOD_UNIT['QUARTER']:
+            sales_by_4prd = EconoIndicator.get_4prd_sum(KEY_NAME['SALES'], tg_period, temp_result, False)
+            op_by_4prd = EconoIndicator.get_4prd_sum(KEY_NAME['OP'], tg_period, temp_result, False)
+            np_by_4prd = EconoIndicator.get_4prd_sum(KEY_NAME['NP_CTRL'], tg_period, temp_result, False)
+
+            PSR = EconoIndicator.get_mltp(data[KEY_NAME['MV']], sales_by_4prd)
+            POR = EconoIndicator.get_mltp(data[KEY_NAME['MV']], op_by_4prd)
+            PER = EconoIndicator.get_mltp(data[KEY_NAME['MV']], np_by_4prd)
+
+        tg_period_data_in_result[KEY_NAME['PSR']] = PSR
+        tg_period_data_in_result[KEY_NAME['POR']] = POR
+        tg_period_data_in_result[KEY_NAME['PER']] = PER
 
     return result
