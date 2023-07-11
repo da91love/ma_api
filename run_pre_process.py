f976352894e11d27e6d410734743cccbb1b354eb
daseul.kim <da91love@gmail.com>
2023-07-12 00:09:29 +0900

    [update]230711 data update

--- run_pre_process.py
+++ run_pre_process.py
@@ -6,6 +6,6 @@ from pre_process.create_mrkcap_data_wrapper import create_mrkcap_data_wrapper
 # API에서 매회 연산하기에는 리소스가 아깝고, 한번 계산해 놓으면 쭉 사용할 수 있는 데이터 가공
 def run_pre_process():
     create_mrkcap_data_wrapper()
-    # create_gdp_data()
-    # create_market_summary_data_wrapper()
+    create_gdp_data()
+    create_market_summary_data_wrapper()
     create_summary_data_wrapper()
